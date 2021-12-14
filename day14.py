f73d47a5d939a6071b7674b4b0ed8ee12f891ffb
anon <n/a>
2021-12-14 10:03:01 +0100

    day 14 (optimized)

--- day14.py
+++ day14.py
@@ -1,7 +1,4 @@
-from collections import Counter
 from itertools import product
-
-import numpy as np
 
 from puzzle import Puzzle
 
@@ -17,28 +14,64 @@ class Day14(Puzzle):
         rules = dict(tuple(line.split(' -> ')) for line in input)
         self.rules = rules
 
-        self.all_letters = set(self.rules.values())
-        self.all_letters |= set(x[0] for x in self.rules.keys())
-        self.all_letters |= set(x[1] for x in self.rules.keys())
+        self.all_letters = set(rules.values())
+        self.all_letters |= set(x[0] for x in rules.keys())
+        self.all_letters |= set(x[1] for x in rules.keys())
+
+        self.all_pairs = {a + b: 0 for a, b in product(self.all_letters, self.all_letters)}
 
         return template
-
-    def insertion_step(self, template):
-        output = [template[0]]
-        for a, b in zip(template, template[1:]):
-            c = self.rules.get(a+b, '')
-            output.append(c)
-            output.append(b)
-        return ''.join(output)
 
     def solve(self, input):
         template = self.load(input)
 
-        for _ in range(self.n_steps):
-            template = self.insertion_step(template)
+        # optimized based on the observation that the actual locations of the letters in the string does not matter.
+        # what matters is just the number of letter-pairs, which produce new letters...
 
-        counts = Counter(template).most_common()
-        return counts[0][1] - counts[-1][1]
+        pair_graph = self.compute_pair_graph()
+        pair_counts = count(map(lambda p: p[0] + p[1], zip(template, template[1:])), self.all_pairs)
+        letter_counts = count(template, self.all_letters)
+
+        for _ in range(self.n_steps):
+            pair_increments = {}
+            for pair, n in pair_counts.items():
+                if n == 0:
+                    continue
+
+                a, c, b = pair_graph[pair]
+                left, right = a + c, c + b
+
+                # for every pair in the current string ...
+
+                # ... insert a new letter in the middle ...
+                letter_counts[c] += n
+
+                # ... creating two new pairs ...
+                pair_increments[left] = pair_increments.get(left, 0) + n
+                pair_increments[right] = pair_increments.get(right, 0) + n
+
+                # ... and destroying the original pair
+                pair_increments[pair] = pair_increments.get(pair, 0) - n
+
+            for p, i in pair_increments.items():
+                pair_counts[p] += i
+
+        counts = [x for x in letter_counts.values() if x > 0]
+        return max(counts) - min(counts)
+
+    def compute_pair_graph(self):
+        rule_graph = {}
+        for a, b in product(self.all_letters, self.all_letters):
+            c = self.rules[a+b]
+            rule_graph[a + b] = (a, c, b)
+        return rule_graph
+
+
+def count(within, all_items):
+    counts = {x: 0 for x in all_items}
+    for x in within:
+        counts[x] += 1
+    return counts
 
 
 def main():
