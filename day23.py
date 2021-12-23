efc93440e4ad9a45aa6de1f123fba17c1b5afc26
Martin Billinger <flkazemakase@gmail.com>
2021-12-23 22:17:01 +0100

    day 23

--- day23.py
+++ day23.py
@@ -1,126 +1,176 @@
-from dataclasses import dataclass
+from collections import deque
+import heapq
 
 from puzzle import Puzzle
 
 
 class Day23(Puzzle):
-    def __init__(self, part):
+    def __init__(self, input, part):
         super().__init__(f"Day 23, Part {part}")
+        self.input = input
+
+    def solve(self, _):
+        world = World.parse(self.input)
+
+        all_costs = {world: 0}
+        visited = set()
+
+        minimum_cost = float("inf")
+        queue = [(0, world)]
+        heapq.heapify(queue)
+        while queue:
+            total_cost, world = heapq.heappop(queue)
+
+            if world in visited:
+                continue
+            visited.add(world)
+
+            if world.done():
+                minimum_cost = min(minimum_cost, total_cost)
+                return minimum_cost
+
+            for cost, after_move in world.all_moves():
+                nc = total_cost + cost
+                if after_move not in all_costs or nc < all_costs[after_move]:
+                    all_costs[after_move] = nc
+                    heapq.heappush(queue, (nc, after_move))
+        return minimum_cost
 
 
 class Part1(Day23):
-    def __init__(self, part=1):
-        super().__init__(part)
+    def __init__(self, input, part=1):
+        super().__init__(input, part)
 
-    def solve(self, input):
-        world = World(input)
-        print(world.possible_moves(0))
+
+class Part2(Day23):
+    def __init__(self, input, part=1):
+        super().__init__(input, part)
 
 
 class World:
-    def __init__(self, input):
-        self.walls = set()
-        self.creatures = []
-        for row, line in enumerate(input):
-            for col, ch in enumerate(line):
-                if ch in "ABCD":
-                    self.creatures.append((ch, (row, col)))
-                if ch == "#":
-                    self.walls.add((row, col))
+    def __init__(self, hallway, rooms, room_size):
+        self.hallway = hallway
+        self.rooms = rooms
+        self.room_size = room_size
 
-    def possible_moves(self, creature_idx):
-        kind, start_pos = self.creatures[creature_idx]
-        started_in_hallway = start_pos == 1
+    def __lt__(self, other):
+        return False
 
-        final_positions = []
+    def __eq__(self, other):
+        return self.hallway == other.hallway and self.rooms == other.rooms
 
-        queue = [(0, start_pos)]
-        visited = set()
-        while queue:
-            cost, pos = queue.pop()
+    def __hash__(self):
+        return hash(self.hallway) ^ hash(self.rooms)
 
-            if pos in visited:
-                continue
+    @staticmethod
+    def parse(input):
+        return World(INITIAL_HALLWAY, tuple(input), room_size=len(input[0]))
 
-            visited.add(pos)
+    def done(self):
+        if not all(x == "." for x in self.hallway):
+            return False
 
-            if pos in self.walls:
-                continue
-
-            if pos != start_pos and self.occupied(pos):
-                continue
-
-            if self.may_stop(kind, pos, started_in_hallway):
-                final_positions.append((cost, pos))
-
-            for npos in neighbors(pos):
-                queue.append((MOVE_COSTS[kind], npos))
-
-        return final_positions
-
-    def occupied(self, pos):
-        for kind, p in self.creatures:
-            if p == pos:
-                return kind
-        return None
-
-    def may_stop(self, kind, pos, started_in_hallway):
-        if pos[0] == 1:
-            if started_in_hallway:
-                # Once an amphipod stops moving in the hallway, it will stay in that spot until it can move into a room.
+        for kind, room in zip("ABCD", self.rooms):
+            if not all(x == kind for x in room):
                 return False
-
-            if pos in DOORWAY:
-                # Amphipods will never stop on the space immediately outside any room.
-                return False
-
-        elif pos[0] > 1:
-            if pos not in TARGETS[kind]:
-                # only move into the designated room
-                return False
-
-            if pos[0] == 2:
-                other = self.occupied((3, pos[1]))
-                if not other:
-                    # no point in blocking the door... must move to the end of the room
-                    return False
-
-                if other != kind:
-                    # still occupied by the wrong kind
-                    return False
 
         return True
 
+    def all_moves(self):
+        for r, x in enumerate(ROOMS_X):
+            yield from self.move_to_hallway(r, x)
 
-def neighbors(pos):
-    y, x = pos
-    yield y, x + 1
-    yield y, x - 1
-    yield y + 1, x
-    yield y - 1, x
+        for i, h in enumerate(self.hallway):
+            yield from self.move_from_hallway(i)
+
+    def move_to_hallway(self, room, x):
+        kind = "ABCD"[room]
+        if not self.rooms[room]:
+            return
+        if all(k == kind for k in self.rooms[room]):
+            return
+
+        creature = self.rooms[room][-1]
+        rooms = tuple(ro[:-1] if i == room else ro for i, ro in enumerate(self.rooms))
+
+        def to_hallway(rng):
+            for p in rng:
+                if self.hallway[p] != ".":
+                    break
+                if p not in ROOMS_X:
+                    hallway = self.set_hallway(p, creature)
+                    distance = self.distance(p, room)
+                    yield distance * MOVE_COSTS[creature], World(
+                        hallway, rooms, self.room_size
+                    )
+
+        yield from to_hallway(range(x, -1, -1))
+        yield from to_hallway(range(x, 11))
+
+    def move_from_hallway(self, p):
+        creature = self.hallway[p]
+        if creature == ".":
+            return
+
+        room = {"A": 0, "B": 1, "C": 2, "D": 3}[creature]
+        if any(k != creature for k in self.rooms[room]):
+            return
+
+        a = min(p + 1, ROOMS_X[room])
+        b = max(p - 1, ROOMS_X[room]) + 1
+        if any(k != "." for k in self.hallway[a:b]):
+            return
+
+        rooms = tuple(
+            ro + (creature,) if i == room else ro for i, ro in enumerate(self.rooms)
+        )
+        hallway = self.set_hallway(p, ".")
+        distance = self.distance(p, room) - 1
+        yield distance * MOVE_COSTS[creature], World(hallway, rooms, self.room_size)
+
+    def distance(self, hallway_pos, room_idx):
+        return (
+            1
+            + abs(hallway_pos - ROOMS_X[room_idx])
+            + self.room_size
+            - len(self.rooms[room_idx])
+        )
+
+    def set_hallway(self, p, item):
+        return self.hallway[:p] + item + self.hallway[p + 1 :]
+
+    def __str__(self):
+        return self.hallway + "\n" + "  | | | |  " + "\n" + str(self.rooms)
 
 
-MOVE_COSTS = {'A': 1, 'B': 10, 'C': 100, 'D': 1000}
-TARGETS = {'A': ((2, 3), (3, 3)), 'B': ((2, 5), (3, 5)), 'C': ((2, 7), (3, 7)), 'D': ((2, 9), (3, 9))}
-DOORWAY = [(1, 3), (1, 5), (1, 6), (1, 7)]
+MOVE_COSTS = {"A": 1, "B": 10, "C": 100, "D": 1000}
+ROOMS_X = (2, 4, 6, 8)
+INITIAL_HALLWAY = "..........."
 
 
 def main():
-    Part1().check(EXAMPLE_INPUT, 12521)
-    Part1().run(ACTUAL_INPUT)
+    Part1(EXAMPLE_INPUT).check("", 12521)
+    Part1(ACTUAL_INPUT).run()
+
+    Part2(EXAMPLE_INPUT2).check("", 44169)
+    Part2(ACTUAL_INPUT2).run()
 
 
 if __name__ == "__main__":
-    EXAMPLE_INPUT = """#############
-#...........#
-###B#C#B#D###
-  #A#D#C#A#
-  #########"""
+    EXAMPLE_INPUT = [("A", "B"), ("D", "C"), ("C", "B"), ("A", "D")]
+    ACTUAL_INPUT = [("B", "D"), ("C", "A"), ("B", "C"), ("A", "D")]
 
-    ACTUAL_INPUT = """#############
-#...........#
-###D#A#C#D###
-  #B#C#B#A#
-  #########"""
+    EXAMPLE_INPUT2 = [
+        ("A", "D", "D", "B"),
+        ("D", "B", "C", "C"),
+        ("C", "A", "B", "B"),
+        ("A", "C", "A", "D"),
+    ]
+    ACTUAL_INPUT2 = [
+        ("B", "D", "D", "D"),
+        ("C", "B", "C", "A"),
+        ("B", "A", "B", "C"),
+        ("A", "C", "A", "D"),
+    ]
 
     main()
--- puzzle.py
+++ puzzle.py
@@ -1,19 +1,23 @@
+from datetime import datetime
+
 
 class Puzzle:
     def __init__(self, name):
         self.name = name
 
     def check(self, input, expected):
+        start = datetime.now()
         result = self.solve(iter(input.splitlines()))
         if result == expected:
-            print(f"OK  {result}")
+            print(f"{datetime.now() - start} -- OK  {result}")
         else:
             raise AssertionError(f"expected {expected}, got {result}")
 
     def run(self, filename=None, wrong=()):
+        start = datetime.now()
         if filename is not None:
             with open(filename, "rt") as f:
-                lines = (l.strip('\n') for l in f)
+                lines = (l.strip("\n") for l in f)
                 result = self.solve(lines)
         else:
             lines = iter("")
@@ -22,5 +26,4 @@ class Puzzle:
         if result in wrong:
             raise AssertionError(f"wrong result: {result}")
 
-        print(f"{self.name}: {result}")
-
+        print(f"{datetime.now() - start} -- {self.name}: {result}")
